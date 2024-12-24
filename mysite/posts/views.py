bb160adace8575426e9341ca21566e925fb06420
정필성 <ibetter.kr@gmail.com>
2024-12-24 15:26:18 +0900

    코드 수정

--- mysite/posts/views.py
+++ mysite/posts/views.py
@@ -9,7 +9,6 @@ from django.contrib import messages
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
-from django.core import serializers
 
 from .models import Posts
 from comments.models import Comments
